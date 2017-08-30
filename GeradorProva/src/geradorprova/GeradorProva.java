2d35ba519b14faacd9d3bd2505234be8ba745f92
mathiasbelle <mathiasbelle@hotmail.com>
2017-08-30 18:04:13 -0300

    Versão final

--- GeradorProva/src/geradorprova/GeradorProva.java
+++ GeradorProva/src/geradorprova/GeradorProva.java
@@ -41,6 +41,7 @@ import java.util.ArrayList;
                 if(decisao.equalsIgnoreCase("x")){
                     break;
                 }else if(decisao.equalsIgnoreCase("o")){
+                    
                     // criar uma questão objetiva
                     Objetiva objetivas=new Objetiva();
                     String[] troca=new String[5];
@@ -63,6 +64,7 @@ import java.util.ArrayList;
                     p.getListaQuestoes().add(objetivas);
                     
                 }else if(decisao.equalsIgnoreCase("d")){
+                    
                     // criar discursiva
                     Discursiva disc;
                     disc=new Discursiva();
@@ -86,48 +88,6 @@ import java.util.ArrayList;
                 
             }
 
-//            for (int i = 0; i < qtd; i++) {
-//                disc=new Discursiva();
-//                System.out.print("Digite a pergunta da "+(i+1)+"ª questão: ");
-//                
-//                disc.setPergunta(s.nextLine());
-//                System.out.print("Digite o peso da "+(i+1)+"ª questão: ");
-//                disc.setPeso(s.nextDouble());
-//                s.nextLine();
-//                System.out.print("Digite o critério de avaliação da "+(i+1)+"ª questão: ");
-//                disc.setCriterioCorrecao(s.nextLine());
-//            }
-//            p.setDiscursivas(disc);
-//            
-//            
-//            
-//            System.out.print("Entre com a quantidade de questões objetivas: ");
-//            qtd=s.nextInt();
-//            s.nextLine();
-//            String[] troca=new String[5];
-//            Objetiva[] objetivas=new Objetiva[qtd];
-//
-//            for (int i = 0; i < qtd; i++) {
-//                objetivas[i]=new Objetiva();
-//                System.out.print("Entre com a pergunta da "+(i+1)+"ª questão: ");
-//                objetivas[i].setPergunta(s.nextLine());
-//
-//                
-//                for (int j = 0; j < 5; j++) {
-//                     System.out.print("Entre com a "+(j+1)+"ª alternativa da "+(i+1)+" questão: ");
-//                     troca[j]=s.nextLine();
-//                }
-//                objetivas[i].setOpcoes(troca);
-//                System.out.print("Entre com a resposta correta da "+(i+1)+"ª questão: ");
-//                objetivas[i].setRespostaCorreta(s.nextInt()-1);
-//                s.nextLine();
-//                System.out.print("Entre com o peso da "+(i+1)+" questão: ");
-//                objetivas[i].setPeso(s.nextInt());
-//                s.nextLine();
-//                   
-//            }
-//            p.setObjetivas(objetivas);
-
             String prova=p.obtemProvaImpressao();
 
             System.out.println("\n Impressao Prova:\n"+prova);
@@ -147,11 +107,6 @@ import java.util.ArrayList;
 	  	}catch(Exception e){}
                 
             }
-	  		
-            
-            //p.peso=3;
-            
-            //System.out.println(p.obtemDetalhes());
             JOptionPane.showMessageDialog(null, prova);
         }
        
