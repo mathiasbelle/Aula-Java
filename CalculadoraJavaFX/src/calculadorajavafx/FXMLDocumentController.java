a0e5ee7df663ab3992b1fbef8902feacc7758feb
mathiasbelle <mathiasbelle@hotmail.com>
2017-09-19 07:35:47 -0300

    Calculadora JXML

---
+++ CalculadoraJavaFX/src/calculadorajavafx/FXMLDocumentController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorajavafx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Mathias
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+
+    
+    @FXML
+    private TextField valorCapital;
+    
+    @FXML
+    private TextField valorTaxa;
+    
+    @FXML
+    private TextField valorTempo;
+    
+    @FXML
+    private TextField valorResultado;
+    
+    
+//    
+//    @FXML
+//    private void handleButtonAction(ActionEvent event) {
+//        System.out.println("You clicked me!");
+//        label.setText("Hello World!");
+//    }
+    
+    @FXML
+    private void calcularClick(ActionEvent event) {
+        System.out.println("Clicou!");
+        valorResultado.setText(""+(Double.parseDouble(valorCapital.getText())*Double.parseDouble(valorTaxa.getText())*Double.parseDouble(valorTempo.getText())));
+        
+        
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
